43218f5a437e971a7f43ea862f1a377b6d4247eb
j.moreno <105161249+holland1antari@users.noreply.github.com>
2022-09-26 21:29:59 -0300

    Add files via upload

---
+++ canivete_suiço/python_em_uma_página.py
@@ -0,0 +1,42 @@
+import os
+
+
+def main():
+    print("Olá mundo!")
+    print("Esta é a saudação de alice.")
+    print("Esta é a saudaçõ\'es do bob.")
+
+    foo(5, 10)
+
+    print("=" * 10)
+    teste = "o diretório de trabalho atual é"
+    print(teste + os.getcwd())
+
+ 
+    comidas =  ["maçãs", "laranjas", "chocolates"]
+
+    for comida in comidas:
+        print("eu gosto de comer", comida)
+    
+    print("conte até dez:")
+    for i in range(1, 11):
+        print(i)
+
+def foo(a, b ):
+    valor = a + b
+
+    print("%s mais %s é igual a %s" % (a, b, valor))
+
+    if valor < 50:
+        print("foo")
+    elif (valor  >= 50) and ((a == 42) or (b == 24)):
+         print("bar")
+    else:
+        print("moo")
+    """uma string multi_line, mas também pode ser um comentário multi_line"""
+
+    return valor #essa é a unica linha de comentário
+
+
+if __name__ == "__main__":
+    main()
