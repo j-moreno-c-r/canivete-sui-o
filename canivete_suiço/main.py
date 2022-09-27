759c24a3d82aa59b45e4cd4d3e83195a830893cc
j.moreno <105161249+holland1antari@users.noreply.github.com>
2022-09-26 21:51:03 -0300

    Add files via upload

---
+++ canivete_suiço/main.py
@@ -0,0 +1,166 @@
+#truques level 1!!!
+
+
+ilusão_menor = ('''
+Tempo de Conjuração: 1 ação 
+Alcance: 9 metros
+Componentes: V, M (um pouco de lã) 
+Duração: 1 minuto
+                
+Você cria um som ou uma imagem de um objeto, dentro do alcance, que permanece pela duração. A ilusão também termina se você dissipa-la usando uma ação ou conjurar essa magia novamente.
+Se você criar um som, seu volume pode variar entre um sussurro até um grito. Pode ser a sua voz, a voz de outrem, o rugido de um leão, batidas de tambor ou qualquer outro som que você quiser. O som permanece no mesmo volume durante toda duração ou você pode fazer sons distintos em momentos diferentes, antes da magia acabar.
+Se você criar uma imagem de um objeto – como uma cadeira, pegadas de lama ou um pequeno baú – ela não pode ter mais de 1,5 metro cúbico. A imagem não pode produzir som, luz, cheiro ou qualquer outro efeito sensorial. Interação física com a imagem revelará que ela é uma ilusão, já que as coisas podem atravessa-la.
+Se uma criatura usar sua ação para examinar a imagem, ela pode determinar que ela é uma ilusão se obtiver sucesso num teste de Inteligência (Investigação) contra a CD da magia. Se uma criatura discernir a ilusão como sendo isso, a ilusão se tornará suave para a criatura.''')
+
+raio_de_fogo = ('''
+Tempo de Conjuração: 1 ação 
+Alcance: 36 metros 
+Componentes: V, S
+Duração: Instantânea
+                
+Você arremessa um cisco de fogo em uma criatura ou objeto dentro do alcance. Faça um ataque à distância com magia contra o alvo. Se atingir, o alvo sofre 1d10 de dano de fogo. Um objeto inflamável atingido por essa magia, incendeia se não estiver sendo vestido ou carregado.
+O dano dessa magia aumenta em 1d10 quando você alcança o 5° nível (2d10), 11° nível (3d10) e 17° nível (4d10).''')
+
+rajada_de_veneno = ('''
+Tempo de Conjuração: 1 ação 
+Alcance: 3 metros 
+Componentes: V, S
+Duração: Instantânea
+                    
+Você ergue sua mão em direção de uma criatura que você possa ver, dentro do alcance e projeta um sopro de gás tóxico da sua palma. A criatura deve ser bem sucedida num teste de resistência de Constituição ou sofrerá 1d12 de dano de veneno.
+O dano dessa magia aumenta em 1d12 quando você alcança o 5° nível (2d12), 11° nível (3d12) e 17° nível (4d12).''')
+
+toque_chocante = ('''
+Tempo de Conjuração: 1 ação 
+Alcance: Toque 
+Componentes: V, S
+Duração: Instantânea
+                  
+Eletricidade surge da sua mão para transmitir um choque em uma criatura que você tentar tocar. Faça um ataque corpo-a-corpo com magia contra o alvo. Você tem vantagem na jogada de ataque se o alvo estiver vestindo qualquer armadura de metal. Se atingir, o alvo sofre 1d8 de dano elétrico e não pode usar reações até o início do próximo turno dele.
+O dano da magia aumenta em 1d8 quando você alcança o 5° nível (2d8), 11° nível (3d8) e 17° nível (4d8).''')
+
+truques_lv_1 = (ilusão_menor,raio_de_fogo,rajada_de_veneno,toque_chocante)#tupla
+
+#magias level 1!!!
+
+# 8 slots!!!
+
+armadura_arcana = ('''
+Tempo de Conjuração: 1 ação
+Alcance: Toque
+Componentes: V, S, M (um pedaço de couro curado) 
+Duração: 8 horas
+                   
+Você toca uma criatura voluntária que não esteja vestindo armadura e uma energia mágica protetora a envolve até a magia acabar. A CA base do alvo se torna 13 + o modificador de Destreza dele. A magia acaba se o alvo colocar uma armadura ou se você dissipa-la usando uma ação.''')
+
+convocar_familiar = ('''
+Tempo de Conjuração: 1 hora
+Alcance: 3 metros
+Componentes: V, S, M (carvão, incenso e ervas no valor
+de 10 po, que devem ser consumidos pelo fogo em um
+braseiro de bronze) 
+Duração: Instantânea
+                     
+Você adquire os serviços de uma familiar, um espírito que toma a forma de um animal, à sua escolha: aranha, caranguejo, cavalo marinho, coruja, corvo, doninha, gato, falcão, lagarto, morcego, peixe (arenque), polvo, rato, rã (sapo) ou serpente venenosa. Aparecendo em um espaço desocupado dentro do alcance, o familiar tem as mesmas estatísticas da forma escolhida, no entanto, ele é um celestial, corruptor ou fada (à sua escolha) ao invés de uma besta.
+Seu familiar até independentemente de você, mas ele sempre obedece aos seus comandos. Em combate, ele rola sua a própria iniciativa e age no seu próprio turno. Um familiar não pode atacar, mas ele pode realizar outras ações, como de costume.
+''')
+
+leque_cromático = ('''
+Tempo de Conjuração: 1 ação
+Alcance: Pessoal (cone de 4,5 metros)
+Componentes: V, S, M (um punhado de pó ou areia nas
+cores vermelha, amarela e azul) 
+Duração: 1 rodada
+                   
+Um feixe ofuscante de luzes coloridas ordenadas, surge da sua mão. Role 6d10; o total é a quantidade de pontos de vida de criaturas que essa magia pode afetar. As criaturas num cone de 4,5 metros, originado de você, são afetadas em ordem ascendente dos seus pontos de vida (ignorando criaturas inconsciente e que não podem ver).
+Começando com as criaturas que tiverem menos pontos de vida, cada criatura afetada por essa magia ficará cega até o fim da magia. Subtraia os pontos de vida de cada criatura do total antes de considerar os pontos de vida da próxima criatura. Os pontos de vida de uma criatura devem ser iguais ou menores que o total restante para que essa criatura seja afetada
+Em Níveis Superiores. Se você conjurar essa magia usando um espaço de magia de 2° nível ou superior, jogue 2d10 adicionais para cada nível do espaço acima do 1°.''')
+
+mãos_flamejantes = ('''
+Tempo de Conjuração: 1 ação 
+Alcance: Pessoal (cone de 4,5 metros) 
+Componentes: V, S
+Duração: Instantânea
+                    
+Enquanto você mantiver suas mãos com os polegares juntos e os dedos abertos, uma fino leque de chamas emerge das pontas dos seus dedos erguidos. Cada criatura num cone de 4,5 metros deve realizar um teste de  resistência de Destreza. Uma criatura sofre 3d6 de dano de fogo se falhar no teste, ou metade desse dano se obtiver sucesso.
+O fogo incendeia qualquer objeto inflamável na área que não esteja sendo vestido ou carregado.
+Em Níveis Superiores. Se você conjurar essa magia usando um espaço de magia de 2° nível ou superior, o dano aumenta em 1d6 para cada nível do espaço acima do 1°.''')
+
+mísseis_mágicos = ('''
+Tempo de Conjuração: 1 ação 
+Alcance: 36 metros 
+Componentes: V, S
+Duração: Instantânea
+                   
+Você cria três dardos brilhantes de energia mística. Cada dardo atinge uma criatura, à sua escolha, que você possa ver, dentro do alcance. Um dardo causa 1d4 + 1 de dano de energia ao alvo. Todos os dardos atingem simultaneamente e você pode direciona-los para atingir uma criatura ou várias. 
+Em Níveis Superiores. Quando você conjurar essa magia usando um espaço de magia de 2° nível ou superior, a magia cria um dardo adicional para cada nível do espaço acima do 1°.''') 
+
+raio_adoecente = ('''
+Tempo de Conjuração: 1 ação 
+Alcance: 18 metros 
+Componentes: V, S
+Duração: Instantânea
+                  
+Um raio adoecente de energia esverdeada chicoteia na direção de uma criatura dentro do alcance. Faça um ataque à distância com magia contra o alvo. Se atingir, o alvo sofrerá 2d8 de dano de veneno e deve realizar um teste de resistência de Constituição. Se falhar na resistência, ele também ficará envenenado até o final do seu próximo turno.
+Em Níveis Superiores. Quando você conjurar essa magia usando um espaço de magia de 2° nível ou superior, o dano da magia aumenta em 1d8 para cada nível do espaço acima do 1°.''')
+
+recuperação_arcana = ("recupera 4 slots de magia")
+
+sono = ('''
+Tempo de Conjuração: 1 ação
+Alcance: 36 metros
+Componentes: V, S, M (um punhado de areia fina,
+pétalas de rosas ou um grilo) 
+Duração: 1 minuto
+        
+Essa magia põem as criaturas num entorpecimento mágico. Jogue 5d8; o total é a quantidade de pontos de vida de criaturas afetados pela magia. As criaturas numa área de 6 metros de raio, centrada no ponto escolhido, dentro do alcance, são afetadas em ordem ascendente dos pontos de vida atuais delas (ignorando criaturas inconscientes).
+Começando com as criaturas com menos pontos de vida atuais, cada criatura afetada por essa magia cai inconsciente até a magia acabar, sofrer dano ou alguém usar sua ação para sacudi-la ou esbofeteá-la até acordar. Subtraia os pontos de vida de cada criatura do total antes de seguir para a próxima criatura com menos pontos de vida atuais. Os pontos de vida atuais da criatura devem ser iguais ou menores que o valor restante para que a criatura possa ser afetada.
+Mortos-vivos e criaturas imunes a serem enfeitiçadas não são afetadas por essa magia.
+Em Níveis Superiores. Quando você conjurar essa magia usando um espaço de magia de 2o nível ou superior, jogue 2d8 adicionais para cada nível do espaço acima do 1°.''') 
+
+magias_lv_1 = (armadura_arcana,convocar_familiar,leque_cromático,mãos_flamejantes,mísseis_mágicos, raio_adoecente,recuperação_arcana,sono)#tupla
+
+print("""você possui os seguintes truques de lv 1:
+ilusão_menor,raio_de_fogo,rajada_de_veneno,toque_chocante   
+                 \/
+                 \/
+você possui as seguintes magias de lv 1:
+armadura_arcana,convocar_familiar,leque_cromático,mãos_flamejantes,mísseis_mágicos, raio_adoecente,recuperação_arcana,sono""")
+print("=:=" * 20)
+
+esc = 0
+
+while esc != "007": 
+  print("=:="*20)
+  esc = str(input("digite o numero da magia ou truque e antes digite (a) para magia e (t) para truque(para fechar digite 007):"))
+  
+  if esc == "t1":#4 truques
+    print(truques_lv_1 [0])
+  elif esc == "t2":
+    print(truques_lv_1 [1])
+  elif esc == "t3":
+    print(truques_lv_1 [2])
+  elif esc == "t4":
+    print(truques_lv_1 [3])
+  elif esc== "a1":#8 magias
+    print(magias_lv_1 [0])
+  elif esc == "a2":
+    print(magias_lv_1 [1])
+  elif esc == "a3":
+    print(magias_lv_1[2])
+  elif esc == "a4":
+    print(magias_lv_1[3])
+  elif esc == "a5":
+    print(magias_lv_1[4])
+  elif esc == "a6":
+    print(magias_lv_1 [5])
+  elif esc == "a7":
+    print(magias_lv_1[6])
+  elif esc == "a8":
+    print(magias_lv_1[7])
+  elif esc == "007":
+    print("você fechou o programa")
+  else:
+    print("inválido")
+print("programa fechado")    
+  
